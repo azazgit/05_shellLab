28522106623333ef5e08d47b2ae474795eefa332
azazgit <anoshzahir@hotmail.com>
2021-06-07 11:53:35 +0100

    eval func: logic for builtin_cmd

--- tsh.c
+++ tsh.c
@@ -12,6 +12,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <csapp.h>
 
 /* Misc manifest constants */
 #define MAXLINE    1024   /* max line size */
@@ -53,6 +54,9 @@ struct job_t jobs[MAXJOBS]; /* The job list */
 
 
 /* Function prototypes */
+
+/* Helper functions I created. */
+
 
 /* Here are the functions that you will implement */
 void eval(char *cmdline);
@@ -165,6 +169,28 @@ int main(int argc, char **argv)
 */
 void eval(char *cmdline) 
 {
+    
+    char * argv[MAXARGS];       /* Argument list execve(). */
+    char buf[MAXLINE];          /* Holds modified command line. */
+    int bg;                     /* Should the job run in bg or fg? */
+    pid_t pid;                  /* Process id */
+
+    strcpy(buf, cmdline);
+    bg = parseline(buf, argv);
+    if (argv[0] == NULL){
+        return;                 /* Ignore empty lines. */
+    }
+
+    if (!builtin_cmd(argv)) {
+        if((pid = Fork()) == 0) { /* Child runs user job. */
+            if (execve(argv[0], argv, environ) < 0) {
+                printf("%s: Command not found.\n", argv[0]);
+                exit(0);
+            }
+        }
+    }
+    
+    
     return;
 }
 
